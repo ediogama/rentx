197f4fec033298ce7c91417fb9f4ed54d38a1005
Edio Gama <ediogama@hotmail.com>
2022-07-13 22:20:56 -0300

    add changes in routes and add list categories and findByName

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,27 +1,27 @@
 import { Router } from "express";
 
-import { Category } from "../model/Category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
 
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.get("/", (request, response) => {
-  return response.status(200).json(categories);
+  const categories = categoriesRepository.list();
+
+  return response.json(categories);
 });
 
 categoriesRoutes.post("/", (request, response) => {
   const { name, description } = request.body;
 
-  const category: Category = new Category();
+  const categoryAlreadyExists = categoriesRepository.findByName(name);
 
-  Object.assign(category, {
-    name,
-    description,
-    created_at: new Date(),
-  });
+  if (categoryAlreadyExists) {
+    return response.status(400).json({ error: "Category Already exists" });
+  }
 
-  categories.push(category);
+  categoriesRepository.create({ name, description });
 
   return response.status(201).send();
 });
