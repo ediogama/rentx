252f8772d34f441b5d71754e43e11b34b7ad6cb3
Edio Gama <ediogama@hotmail.com>
2022-07-14 21:38:47 -0300

    add createSpecificationsService, SpecificationsRepository, ISpecificationsRepository and router

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,5 +1,23 @@
+import { ISpecificationsRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() { }
+  constructor(private specificationsRepository: ISpecificationsRepository) { }
+
+  execute({ name, description }: IRequest) {
+    const specificationAlreadyExists =
+      this.specificationsRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification Already exists");
+    }
+
+    this.specificationsRepository.create({ name, description });
+  }
 }
 
 export { CreateSpecificationService };
