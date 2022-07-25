9e9688faa31adc0238f43e1a4c060282541085da
Edio Gama <ediogama@hotmail.com>
2022-07-24 21:59:42 -0300

    add seed admin

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,25 @@
+import { hash } from "bcrypt";
+import { v4 as uuidV4 } from "uuid";
+
+import { PostgresDataSource } from "../data-source";
+
+async function createUserAdmin() {
+  const id = uuidV4();
+  const password = await hash("admin", 8);
+
+  await PostgresDataSource.driver.connect();
+
+  const queryRunner = PostgresDataSource.createQueryRunner();
+
+  await queryRunner.connect();
+
+  await queryRunner.query(
+    `INSERT INTO USERS(id, name, email, password, "isAdmin", created_at, driver_license) values('${id}', 'admin', 'admin@rentx.com', '${password}', true, 'now()', 'XXXXXXX')`
+  );
+
+  await PostgresDataSource.destroy();
+}
+
+createUserAdmin()
+  .then(() => console.log("User Admin Created!"))
+  .catch((error) => console.log(error));
