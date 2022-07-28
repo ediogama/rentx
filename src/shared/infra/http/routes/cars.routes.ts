f9a45a7305c09340335623da96a40b4efaac1031
Edio Gama <ediogama@hotmail.com>
2022-07-27 21:33:27 -0300

    add UploadCarImages and Entity with repository and route

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,23 +1,38 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "@config/upload";
 import { CreateCarController } from "@modules/cars/useCases/createCar/CreateCarController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
+import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImages/UploadCarImagesController";
 
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const carsRoutes = Router();
 
+const upload = multer(uploadConfig.upload("./tmp/cars"));
+
 const createCarController = new CreateCarController();
 
 const createCarSpecificationController = new CreateCarSpecificationController();
+
+const uploadCarImagesController = new UploadCarImagesController();
 
 const listAvailableCarsController = new ListAvailableCarsController();
 
 carsRoutes.post("/", ensureAuthenticated, ensureAdmin, createCarController.handle);
 
 carsRoutes.post("/specifications/:id", ensureAuthenticated, ensureAdmin, createCarSpecificationController.handle);
+
+carsRoutes.post(
+  "/images/:id",
+  ensureAuthenticated,
+  ensureAdmin,
+  upload.array("images"),
+  uploadCarImagesController.handle
+);
 
 carsRoutes.get("/available", listAvailableCarsController.handle);
 
--- src/shared/infra/typeorm/data-source.ts
+++ src/shared/infra/typeorm/data-source.ts
@@ -2,6 +2,7 @@ import { DataSource } from "typeorm";
 
 import { User } from "@modules/accounts/infra/typeorm/entities/User";
 import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+import { CarImage } from "@modules/cars/infra/typeorm/entities/CarImage";
 import { Category } from "@modules/cars/infra/typeorm/entities/Category";
 import { Specification } from "@modules/cars/infra/typeorm/entities/Specification";
 
@@ -12,7 +13,7 @@ export const PostgresDataSource = new DataSource({
   username: "databasePG",
   password: "testdatabase",
   database: "postgres",
-  entities: [Specification, Category, User, Car],
+  entities: [Specification, Category, User, Car, CarImage],
   migrations: ["./src/shared/infra/typeorm/migrations/*.ts"],
 });
 
