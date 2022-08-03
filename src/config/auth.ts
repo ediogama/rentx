32cc7118558846955782d50303a6c87c92a8f765
Edio Gama <ediogama@hotmail.com>
2022-08-03 20:16:33 -0300

    add some changes

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,4 +1,7 @@
 export default {
   secret_token: "dad11cfef8f1f8c78bb23962643aa0e865f60dc5",
+  secret_refresh_token: "bbc37e0ac3645524c41577cffdaede5a",
   expires_in_token: "15m",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
 };
