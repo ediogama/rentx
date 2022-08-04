03ca9f42ff6b6cf1ec81f336fa07cfdfa86b36e1
Edio Gama <ediogama@hotmail.com>
2022-08-03 22:15:41 -0300

    fix class name

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 
-import { ListCategoeriesUseCase } from "./ListCategoriesUseCase";
+import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 
 class ListCategoriesController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const listCategoriesUseCase = container.resolve(ListCategoeriesUseCase);
+    const listCategoriesUseCase = container.resolve(ListCategoriesUseCase);
 
     const categories = await listCategoriesUseCase.execute();
 
