c24d4d1fd9d1f06c49d80da370a160973e549a39
Edio Gama <ediogama@hotmail.com>
2022-08-03 21:09:05 -0300

    add findByUserIdAndRefreshToken() and deleteById()

--- src/modules/accounts/repositories/IUsersTokensRepository.ts
+++ src/modules/accounts/repositories/IUsersTokensRepository.ts
@@ -3,6 +3,8 @@ import { UsersTokens } from "../infra/typeorm/entities/UsersTokens";
 
 interface IUsersTokensRepository {
   create({ expires_date, refresh_token, user_id }: ICreateUserToken): Promise<UsersTokens>;
+  findByUserIdAndRefreshToken(user_id: string, refresh_token: string): Promise<UsersTokens>;
+  deleteById(id: string): Promise<void>;
 }
 
 export { IUsersTokensRepository };
