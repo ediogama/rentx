08e56a56481db893da1250c6f135e8b4636fa9fb
Edio Gama <ediogama@hotmail.com>
2022-08-03 22:19:38 -0300

    add refresh token in authenticated middlewares

--- src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -1,12 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { verify } from "jsonwebtoken";
-import { container } from "tsyringe";
 
-import { UsersRepository } from "@modules/accounts/infra/typeorm/repositories/UsersRepository";
+import auth from "@config/auth";
+import { UsersTokensRepository } from "@modules/accounts/infra/typeorm/repositories/UsersTokensRepository";
 import { AppError } from "@shared/errors/AppError";
 
 export async function ensureAuthenticated(request: Request, response: Response, next: NextFunction) {
   const authHeader = request.headers.authorization;
+
+  const userTokensRepository = new UsersTokensRepository();
 
   if (!authHeader) {
     throw new AppError("Token missing", 401);
@@ -15,18 +17,18 @@ export async function ensureAuthenticated(request: Request, response: Response, 
   const [, token] = authHeader.split(" ");
 
   try {
-    const { sub: user_id } = verify(token, "dad11cfef8f1f8c78bb23962643aa0e865f60dc5");
+    const { sub: user_id } = verify(token, auth.secret_refresh_token);
 
-    const userRepositorty = container.resolve(UsersRepository);
-
-    const user = await userRepositorty.findById(user_id as string);
+    const user = await userTokensRepository.findByUserIdAndRefreshToken(user_id as string, token);
 
     if (!user) {
       throw new AppError("User does not exists", 401);
     }
+
     request.user = {
       id: user_id as string,
     };
+
     next();
   } catch (error) {
     throw new AppError("Invalid token!", 401);
