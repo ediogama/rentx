156466010f6fa91d9654ee54d390cf254ef1c891
Edio Gama <ediogama@hotmail.com>
2022-08-10 11:47:46 -0300

    add S3StorageProvider

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -4,11 +4,11 @@ import { IDateProvider } from "./DateProvider/IDateProvider";
 import { DayJsDateProvider } from "./DateProvider/implementations/DayJsDateProvider";
 import { IMailProvider } from "./MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
-import { LocalStorageProvider } from "./StorageProvider/implementations/LocalStorageProvider";
+import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
 import { IStorageProvider } from "./StorageProvider/IStorageProvider";
 
 container.registerSingleton<IDateProvider>("DayJsDateProvider", DayJsDateProvider);
 
 container.registerInstance<IMailProvider>("EtherealMailProvider", new EtherealMailProvider());
 
-container.registerSingleton<IStorageProvider>("StorageProvider", LocalStorageProvider);
+container.registerSingleton<IStorageProvider>("StorageProvider", S3StorageProvider);
