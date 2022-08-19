fe90e1106ec27eed6fd9621e294edd8c1623a51f
Edio Gama <ediogama@hotmail.com>
2022-08-18 22:00:08 -0300

    refactoring index dependency injection

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,14 +1,3 @@
-import { container } from "tsyringe";
-
-import { IDateProvider } from "./DateProvider/IDateProvider";
-import { DayJsDateProvider } from "./DateProvider/implementations/DayJsDateProvider";
-import { IMailProvider } from "./MailProvider/IMailProvider";
-import { EtherealMailProvider } from "./MailProvider/implementations/EtherealMailProvider";
-import { S3StorageProvider } from "./StorageProvider/implementations/S3StorageProvider";
-import { IStorageProvider } from "./StorageProvider/IStorageProvider";
-
-container.registerSingleton<IDateProvider>("DayJsDateProvider", DayJsDateProvider);
-
-container.registerInstance<IMailProvider>("EtherealMailProvider", new EtherealMailProvider());
-
-container.registerSingleton<IStorageProvider>("StorageProvider", S3StorageProvider);
+import "./DateProvider";
+import "./MailProvider";
+import "./StorageProvider";
