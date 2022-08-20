ce862eec1004f876bf8a162a48552f62274d4a31
Edio Gama <ediogama@hotmail.com>
2022-08-20 00:44:45 -0300

    Delete dist directory

--- dist/src/shared/container/providers/DateProvider/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tsyringe_1 = require("tsyringe");
-const DayJsDateProvider_1 = require("./implementations/DayJsDateProvider");
-tsyringe_1.container.registerSingleton("DayJsDateProvider", DayJsDateProvider_1.DayJsDateProvider);
