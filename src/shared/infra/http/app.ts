765ac39e91d9e3b2a7148037bf202eac2517c33a
Edio Gama <ediogama@hotmail.com>
2022-08-23 21:34:46 -0300

    add cors

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
@@ -23,6 +24,7 @@ app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
+app.use(cors());
 app.use(router);
 
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
